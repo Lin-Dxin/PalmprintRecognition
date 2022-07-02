06c5a897649447ebe3af20a1c808b41eb4e9f72f
DxinLin <wangyilinda66@163.com>
2022-07-02 21:22:18 +0800

    renew_2

--- DataLoad.py
+++ DataLoad.py
@@ -75,38 +75,6 @@ class DataLoader(object):
         return Variable(image_tensor)
 
 
-def loader(path, batch_size, num_workers=4, pin_memory=True):
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    return data.DataLoader(
-        datasets.ImageFolder(path,
-                             transforms.Compose([
-                                 transforms.Resize([122, 244]),
-                                 transforms.RandomResizedCrop([122, 244]),
-                                 transforms.RandomHorizontalFlip(),
-                                 transforms.ToTensor(),
-                                 normalize,
-                             ])),
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        pin_memory=pin_memory)
-
-
-def test_loader(path, batch_size, num_workers=4, pin_memory=True):
-    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-    return data.DataLoader(
-        datasets.ImageFolder(path,
-                             transforms.Compose([
-                                 transforms.Resize([122, 244]),
-                                 transforms.CenterCrop(244),
-                                 transforms.ToTensor(),
-                                 normalize,
-                             ])),
-        batch_size=batch_size,
-        shuffle=False,
-        num_workers=num_workers,
-        pin_memory=pin_memory)
-
 
 if __name__ == '__main__':
     train_dir = "data/TrainingSet/NIR"
