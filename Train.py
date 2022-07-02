747d894082ccef9ec3382b1cc063f725ee43e2b4
lin <506217022@qq.com>
2022-07-02 19:56:00 +0800

    add a gpu solution

--- Train.py
+++ Train.py
@@ -10,9 +10,11 @@ import torch.optim as optim
 from torch.autograd import Variable
 from random import sample
 
-def train_model(data_loader, model, criterion, optimizer, lr_scheduler, num_epochs=25):
+
+def train_model(data_loader, model, criterion, optimizer, lr_scheduler, num_epochs=25, use_gpu=False):
     """
     the pipeline of train PyTorch model
+    :param use_gpu:
     :param data_loader:
     :param model:
     :param criterion:
@@ -44,9 +46,12 @@ def train_model(data_loader, model, criterion, optimizer, lr_scheduler, num_epoc
                 rawinputs, rawlabels = batch_data
 
                 inputs, labels = get_concated_data(rawinputs, rawlabels, 50)
-                inputs, labels = Variable(inputs), Variable(labels)
-                #print(inputs.shape, labels.shape)
-                #print(labels)
+                if use_gpu:
+                    inputs, labels = Variable(inputs.cuda()), Variable(labels.cuda)
+                else:
+                    inputs, labels = Variable(inputs), Variable(labels)
+                # print(inputs.shape, labels.shape)
+                # print(labels)
                 optimizer.zero_grad()
 
                 outputs = model(inputs)
@@ -85,32 +90,33 @@ def get_concated_data(rawinputs, rawlabels, batch_size):
     # cnt 记录拼接次数、一旦超过一半后
     # 随机挑选后面的数字 6000 3000 3000 1500 1500
     # 3000 if cnt > 1500: random_pic (1500,3000)
-    inputs = torch.zeros([25, 3, 244, 244],dtype=torch.float32)
-    labels = torch.zeros(25,dtype=torch.int64)
-    #print(rawlabels.dtype)
-    #print(rawinputs.dtype)
+    inputs = torch.zeros([25, 3, 244, 244], dtype=torch.float32)
+    labels = torch.zeros(25, dtype=torch.int64)
+    # print(rawlabels.dtype)
+    # print(rawinputs.dtype)
     it_times = int(batch_size / 2)
     cnt = 0
     j = 0
-    for i in range(it_times): #  0  2  4
+    for i in range(it_times):  # 0  2  4
         if i < it_times / 2:
             inputs[j] = torch.cat([rawinputs[cnt], rawinputs[cnt + 1]], 1)
             labels[j] = 1 if rawlabels[cnt] == rawlabels[cnt + 1] else 0
-            #print(rawlabels[cnt], rawlabels[cnt + 1])
-            #print("=======label:",j,"++++",labels[j],"=======")
+            # print(rawlabels[cnt], rawlabels[cnt + 1])
+            # print("=======label:",j,"++++",labels[j],"=======")
             j = j + 1
 
             # 顺序抽取
         else:
-            rand_a, rand_b = sample(range(it_times,batch_size,1), 2) # 从后半部分随机抽取两个数字
+            rand_a, rand_b = sample(range(it_times, batch_size, 1), 2)  # 从后半部分随机抽取两个数字
             inputs[j] = torch.cat([rawinputs[rand_a], rawinputs[rand_b]], 1)
             labels[j] = 1 if rawlabels[rand_a] == rawlabels[rand_b] else 0
-            #print(rawlabels[rand_a], rawlabels[rand_b])
-            #print("=======label:", j, "++++", labels[j], "=======")
+            # print(rawlabels[rand_a], rawlabels[rand_b])
+            # print("=======label:", j, "++++", labels[j], "=======")
             j = j + 1
             # 随机抽取
         cnt = cnt + 2
     return inputs, labels
+
 
 def exp_lr_scheduler(optimizer, epoch, init_lr=0.001, lr_decay_epoch=7):
     """Decay learning rate by a factor of 0.1 every lr_decay_epoch epochs."""
@@ -132,17 +138,17 @@ def save_torch_model(model, name):
 if __name__ == '__main__':
     train_dir = "data/TrainingSet/NIR/"
 
-    data_loader = DataLoad.DataLoader(data_dir='Data/TrainingSet/ROI_image/', image_size=[122,244], batch_size=50)
-    #inputs, classes = next(iter(data_loader.load_data()))
-    #out = torchvision.utils.make_grid(inputs)
-    #data_loader.show_image(out, title=[data_loader.data_classes[c] for c in classes])
+    data_loader = DataLoad.DataLoader(data_dir='Data/TrainingSet/ROI_image/', image_size=[122, 244], batch_size=50)
+    # inputs, classes = next(iter(data_loader.load_data()))
+    # out = torchvision.utils.make_grid(inputs)
+    # data_loader.show_image(out, title=[data_loader.data_classes[c] for c in classes])
 
     model = fine_tune_model()
 
     criterion = nn.CrossEntropyLoss()
     optimizer_ft = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
     try:
-        model = train_model(data_loader, model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+        model = train_model(data_loader, model, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25, use_gpu=False)
         save_torch_model(model, "model1")
     except KeyboardInterrupt:
         print('manually interrupt, try saving model for now...')
--- model.py
+++ model.py
@@ -4,11 +4,12 @@ from torch import nn
 from torchvision.models import ResNet18_Weights
 
 
-def fine_tune_model():
+def fine_tune_model(use_gpu=False):
     model_feature = models.resnet18(weights=ResNet18_Weights.DEFAULT)
     # 记录全连接层的特征输入数
     num_features = model_feature.fc.in_features
     # 将最后一层的全连接层修改为我们定义的层
     model_feature.fc = nn.Linear(num_features, 2)
-
+    if use_gpu:
+        model_feature = model_feature.cuda()
     return model_feature
